f92c2bcf9cf42b71f5d44ebcf6cc7fafff00e9f8
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-13 13:11:10 +0530

    Add cache control meta tags and login link to registration page

---
+++ Java/Servlets/RegistrationWebApp/src/main/java/servlet/Home.java
@@ -0,0 +1,22 @@
+
+package servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class Home extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            // Redirect to the registration form
+            response.sendRedirect(request.getContextPath() + "/index.html");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
